2fbc3135daa6168ffc0602d0f3c3152122855f19
Benedikt Franke <benedikt.franke@mll.com>
2019-09-12 18:37:14 +0200

    Add bearer auth to sailor.php

--- sailor.php
+++ sailor.php
@@ -3,14 +3,20 @@
 declare(strict_types=1);
 
 use Spawnia\Sailor\Client;
-use Spawnia\Sailor\GuzzleClient;
 use Spawnia\Sailor\EndpointConfiguration;
 
 return [
     'example' => new class implements EndpointConfiguration {
         public function client(): Client
         {
-            return new GuzzleClient('http://example.com/graphql');
+            return new \Spawnia\Sailor\GuzzleClient(
+                'http://example.com/graphql',
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer foobarbaz'
+                    ]
+                ]
+            );
         }
-    },
+    }
 ];
