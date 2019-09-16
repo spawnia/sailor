9e3cdef491591adffcef37508156cb9cfa752903
Benedikt Franke <benedikt.franke@mll.com>
2019-09-16 09:30:29 +0200

    Fix install test

--- examples/install/test.sh
+++ examples/install/test.sh
@@ -2,7 +2,11 @@
 set -euxo pipefail
 
 composer update
-vendor/bin/sailor
+
+if vendor/bin/sailor; then
+  echo Expected the initial run of vendor/bin/sailor to exit with an error
+  exit 1;
+fi
 
 if ! test -f sailor.php; then
   echo Expected sailor.php to be present after first executing vendor/bin/sailor
