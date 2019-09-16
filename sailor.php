4db13815963cac7e2f4a3d3fa1a583efeb59286a
Benedikt Franke <benedikt.franke@mll.com>
2019-09-16 09:55:01 +0200

    Comment endpoint config

--- sailor.php
+++ sailor.php
@@ -7,6 +7,11 @@ use Spawnia\Sailor\EndpointConfig;
 
 return [
     'example' => new class extends EndpointConfig {
+        /**
+         * The namespace the generated classes will be created in.
+         *
+         * @return string
+         */
         public function makeClient(): Client
         {
             return new \Spawnia\Sailor\Client\Guzzle(
@@ -19,21 +24,41 @@ return [
             );
         }
 
+        /**
+         * The namespace the generated classes will be created in.
+         *
+         * @return string
+         */
         public function namespace(): string
         {
             return 'Vendor\\ExampleApi';
         }
 
+        /**
+         * Path to the directory where the generated classes will be put.
+         *
+         * @return string
+         */
         public function targetPath(): string
         {
             return 'generated/ExampleApi';
         }
 
+        /**
+         * Where to look for .graphql files containing operations.
+         *
+         * @return string
+         */
         public function searchPath(): string
         {
             return 'src';
         }
 
+        /**
+         * The location of the schema file that describes the endpoint.
+         *
+         * @return string
+         */
         public function schemaPath(): string
         {
             return 'example.graphqls';
--- src/EndpointConfig.php
+++ src/EndpointConfig.php
@@ -11,7 +11,40 @@ abstract class EndpointConfig
     /** @var MockClient|null */
     public $mockClient;
 
+    /**
+     * Instantiate a client that will resolve the GraphQL operations.
+     *
+     * @return Client
+     */
     abstract public function makeClient(): Client;
+
+    /**
+     * The namespace the generated classes will be created in.
+     *
+     * @return string
+     */
+    abstract public function namespace(): string;
+
+    /**
+     * Path to the directory where the generated classes will be put.
+     *
+     * @return string
+     */
+    abstract public function targetPath(): string;
+
+    /**
+     * Where to look for .graphql files containing operations.
+     *
+     * @return string
+     */
+    abstract public function searchPath(): string;
+
+    /**
+     * The location of the schema file that describes the endpoint.
+     *
+     * @return string
+     */
+    abstract public function schemaPath(): string;
 
     public function client(): Client
     {
@@ -21,12 +54,4 @@ abstract class EndpointConfig
 
         return $this->makeClient();
     }
-
-    abstract public function namespace(): string;
-
-    abstract public function targetPath(): string;
-
-    abstract public function searchPath(): string;
-
-    abstract public function schemaPath(): string;
 }
