abc571158a9a34c746ddbe0cc2dec80e712bc94b
Benedikt Franke <benedikt.franke@mll.com>
2021-02-15 12:05:45 +0100

    Update deps

---
+++ tests/Unit/Testing/Invokable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Spawnia\Sailor\Tests\Unit\Testing;
+
+/**
+ * Stub class to allow creating a partial mock.
+ */
+class Invokable
+{
+    // @phpstan-ignore-next-line
+    public function __invoke()
+    {
+        //
+    }
+}
