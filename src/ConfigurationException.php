37e3bd99a6e0017cefe6e6eeb8cf385c9c6d1146
Benedikt Franke <benedikt.franke@mll.com>
2021-03-05 15:38:58 +0100

    finalize

--- src/ConfigurationException.php
+++ src/ConfigurationException.php
@@ -4,19 +4,15 @@ declare(strict_types=1);
 
 namespace Spawnia\Sailor;
 
-use Spawnia\Sailor\Console\ConsoleException;
-
-class ConfigurationException extends \Exception
+final class ConfigurationException extends \Exception
 {
     public static function wrongReturnType(string $type): self
     {
         return new self("Expected `sailor.php` to return an array, got: {$type}.");
     }
 
-    public static function missingEndpoint(string $endpoint): ConsoleException
+    public static function missingEndpoint(string $endpoint): self
     {
-        return new ConsoleException(
-            "The given endpoint {$endpoint} does not exist in the configuration. You may want to add it to your `sailor.php`."
-        );
+        return new self("The given endpoint {$endpoint} does not exist in the configuration. You may want to add it to your `sailor.php`.");
     }
 }
