12a065e278e328f99df40c19c60067607025094f
Benedikt Franke <spawnia@users.noreply.github.com>
2021-07-05 13:01:19 +0000

    Apply fixes from StyleCI

--- tests/Unit/Client/LogTest.php
+++ tests/Unit/Client/LogTest.php
@@ -4,19 +4,13 @@ declare(strict_types=1);
 
 namespace Spawnia\Sailor\Tests\Unit\Client;
 
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
-use Spawnia\Sailor\Client\Guzzle;
 use Spawnia\Sailor\Client\Log;
 
 class LogTest extends TestCase
 {
-    const FILENAME = __DIR__ . '/LogTest.log';
-    const EXPECTED_JSON = /** @lang JSON */ '{"query":"{ foo }","variables":{"bar":42}}' . "\n";
+    const FILENAME = __DIR__.'/LogTest.log';
+    const EXPECTED_JSON = /** @lang JSON */ '{"query":"{ foo }","variables":{"bar":42}}'."\n";
 
     protected function tearDown(): void
     {
@@ -40,6 +34,6 @@ class LogTest extends TestCase
         $log->request(/** @lang GraphQL */ '{ foo }', (object) ['bar' => 42]);
 
         $contents = file_get_contents(self::FILENAME);
-        $this->assertSame(self::EXPECTED_JSON . self::EXPECTED_JSON, $contents);
+        $this->assertSame(self::EXPECTED_JSON.self::EXPECTED_JSON, $contents);
     }
 }
