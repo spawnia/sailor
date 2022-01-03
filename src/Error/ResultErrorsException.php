93e922e77ceabac58e898f7b6d16fab8e85340d8
Benedikt Franke <benedikt.franke@mll.com>
2022-01-03 15:55:56 +0100

    add todo

--- src/Error/ResultErrorsException.php
+++ src/Error/ResultErrorsException.php
@@ -5,6 +5,9 @@ namespace Spawnia\Sailor\Error;
 use Exception;
 use GraphQL\Error\ClientAware;
 
+/**
+ * TODO implement \GraphQL\Error\ProvidesExtensions once upgrading to graphql-php:15
+ */
 class ResultErrorsException extends Exception implements ClientAware
 {
     use WrapsEndpointError;
